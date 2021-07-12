9f8eff9b0ec60c4cc768b61b4296b889b96834e7
Gabe-ds <mahiro.sogabe.2019@ds.musashino-u.ac.jp>
2021-07-12 15:29:42 +0900

    move db info to secret file

--- config/settings.py
+++ config/settings.py
@@ -78,17 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aialgorithmdesign',
-        'USER': 'gabe',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '5432',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
